108bcb48c9d68c612114b0b9ce31abddf9bd488f
Setiver <lukasjaniak44@gmail.com>
2023-04-25 12:28:45 +0200

    moving background

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let highscore = 0;
 
 const scoreSelector = document.querySelector('.score');
 const numberSelector = document.querySelector('.number');
-const bodySelector = document.querySelector('body');
+const bodySelector = document.querySelector('body').style.backgroundImage;
 const guessSelector = document.querySelector('.guess');
 
 const displayMessage = function (message) {
@@ -17,29 +17,31 @@ document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(guessSelector.value);
 
   if (!guess) {
-    displayMessage('Not a number');
+    displayMessage('Not a number!');
   }
 
   if (guess === unknownNumber) {
-    displayMessage('You Win');
+    displayMessage('✨You Win✨');
     numberSelector.textContent = unknownNumber;
 
-    bodySelector.style.backgroundImage = 'linear-gradient(rgb(0, 229, 250), rgb(0, 199, 100))';
+    bodySelector = 'linear-gradient(rgb(0, 229, 250), rgb(0, 199, 100))';
 
     if (score > highscore) {
       highscore = score;
       document.querySelector('.highscore').textContent = highscore;
     }
   }
+
   if (guess !== unknownNumber) {
     if (score > 1) {
       displayMessage(guess > unknownNumber ? 'Too much' : 'Too low');
       score--;
       scoreSelector.textContent = score;
     } else {
-      displayMessage(`You lost`);
+      displayMessage('You lost 😥');
       scoreSelector.textContent = 0;
-      bodySelector.style.backgroundImage = 'linear-gradient(rgb(86, 0, 59), rgb(255, 0, 0))';
+      numberSelector.textContent = unknownNumber;
+      bodySelector = 'linear-gradient(-45deg, #ffe9e2, #ff0000, #8600a8, #220035)';
     }
   }
 });
@@ -52,5 +54,5 @@ document.querySelector('.again').addEventListener('click', function () {
   scoreQuery.textContent = 20;
   numberSelector.textContent = '?';
   guessSelector.value = '';
-  bodySelector.style.backgroundImage = 'linear-gradient(rgb(255, 212, 212), rgb(123, 0, 180))';
+  bodySelector = 'llinear-gradient(-45deg, #ff3c01, #00d478, #4acfff, #ffee36)';
 });
